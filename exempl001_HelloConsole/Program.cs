a92f4a2e3826fca5ed980a71c99e69ed4f976d43
NaniManina <nani.manina999@mail.ru>
2022-11-25 20:43:28 +0500

    добавила gitignor

--- exempl001_HelloConsole/Program.cs
+++ exempl001_HelloConsole/Program.cs
@@ -1,2 +1,4 @@
-﻿// Здесь обычно пишется комментарий к коду
-Console.WriteLine("Hello, World!2");
+﻿Console.WriteLine("ВВедите ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
